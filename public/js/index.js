0e4d64c0c0a8d9d9eabb1375e55343dadb91047b
Adil Saju <adilsaju@gmail.com>
2022-07-05 22:57:15 -0700

    eror fix index

--- public/js/index.js
+++ public/js/index.js
@@ -57,11 +57,3 @@ add.addEventListener ('click', () => {
   });
 
 
-//hamburger
-const menuButton = document.querySelector("#menu")
-const navbar = document.querySelector("header>nav")
-
-menuButton.onclick = function() {
-  console.log("toggle")
-  navbar.classList.toggle("hide")
-}
