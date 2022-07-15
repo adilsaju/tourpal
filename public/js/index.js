38fe45408ed68651403a6d4ae176d06bc5bbb971
Adil Saju <adilsaju@gmail.com>
2022-07-14 23:47:30 -0700

    navbar again email

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,4 @@
-window.onload = function() {
-  
-  // console.log("it worked")
-  let is_logged_in = "false"
-  try {
-    is_logged_in = sessionStorage.getItem('is_logged_in');
-    
-  } catch (error) {
-    is_logged_in = "false"
-  }
-  if (is_logged_in === "true") {
-    console.log("user already logged in. no problem")
-    let useremail = sessionStorage.getItem("logged_in_email")
-    console.log(useremail)
-    const logouta = document.querySelector("#logout")
-    logouta.append(useremail)
-  }
-  else
-  {
-    //redirect
-    let host = window.location.host
-    console.log(host)
-    window.location.href = `/public/templates/landing-page.html`
-  }
-}
+
 
 add.addEventListener ('click', (e) => {
   e.preventDefault();
